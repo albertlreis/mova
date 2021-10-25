0d313b50fc8c525cc9954d8ab3c723ff8c9ccd3d
Albert Reis <albertlimar4@gmail.com>
2021-10-25 17:22:27 -0300

    Gerar mensagens

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+setlocale(LC_TIME,"pt-br");
+use Mova\Model\Mensagem;
+use Slim\Slim;
+
+require_once("vendor/autoload.php");
+
+$app = new Slim();
+
+$app->config('debug',true);
+
+$Mensagem = new Mensagem();
+echo $Mensagem->geraMensagem("15/11/2021");
